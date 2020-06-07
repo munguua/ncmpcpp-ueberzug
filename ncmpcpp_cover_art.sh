8d990e375bac52fcbbadf53ec67d6513b6ac5e48
alnj <kokone@takat.su>
2020-06-07 23:59:32 +0200

    fix accidentally repeated code

--- ncmpcpp_cover_art.sh
+++ ncmpcpp_cover_art.sh
@@ -7,20 +7,20 @@ fallback_image="$HOME/.ncmpcpp/ncmpcpp-ueberzug/img/fallback.png"
 padding_top=3
 padding_bottom=1
 padding_right=1
-max_width=0
+max_width=30
 reserved_playlist_cols=30
 reserved_cols_in_percent="false"
 force_square="false"
 square_alignment="top"
 
-left_aligned="false"
+left_aligned="true"
 padding_left=1
 
 # Only set this if the geometries are wrong or ncmpcpp shouts at you to do it.
 # Visually select/highlight a character on your terminal, zoom in an image 
 # editor and count how many pixels a character's width and height are.
-font_height=
-font_width=
+font_height=16
+font_width=7
 
 main() {
     kill_previous_instances >/dev/null 2>&1
@@ -122,8 +122,6 @@ compute_geometry() {
 }
 
 compute_geometry_left_aligned() {
-    compute_geometry_common
-
     ueber_left=$padding_left
     max_width_chars=$(( term_cols * max_width / 100 ))
     if [ "$max_width" != 0 ] &&
@@ -135,8 +133,6 @@ compute_geometry_left_aligned() {
 }
 
 compute_geometry_right_aligned() {
-    compute_geometry_common
-
     if [ "$reserved_cols_in_percent" = "true" ]; then
         ueber_left_percent=$(printf "%.0f\n" $(calc "$ueber_left" / "$term_cols" '*' 100))
         if [ "$ueber_left_percent" -lt "$reserved_playlist_cols" ]; then
