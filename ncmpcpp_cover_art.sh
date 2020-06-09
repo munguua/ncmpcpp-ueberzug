199cbc07dc3154f44ab9910a6630fc38434d0b7e
alnj <kokone@takat.su>
2020-06-09 22:52:33 +0200

    minor

--- ncmpcpp_cover_art.sh
+++ ncmpcpp_cover_art.sh
@@ -14,7 +14,7 @@ force_square="false"
 square_alignment="top"
 
 left_aligned="false"
-padding_left=1
+padding_left=
 
 # Only set this if the geometries are wrong or ncmpcpp shouts at you to do it.
 # Visually select/highlight a character on your terminal, zoom in an image 
